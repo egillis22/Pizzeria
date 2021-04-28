62ead348f18315a57580f0354c217896ae992e39
Erica Gillis <erica_gillis1@baylor.edu>
2021-04-28 17:14:12 -0500

    added homepage layout

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4',
     'pizzas',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
